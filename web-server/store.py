02197a5d600656edfa982711d01ed3e0885e397e
ftalero <fredy.talero@gmail.com>
2023-11-02 08:42:35 -0500

    create web-server

---
+++ web-server/store.py
@@ -0,0 +1,9 @@
+import requests
+
+def get_categories():
+    response = requests.get('https://api.escuelajs.co/api/v1/categories')
+    print(response.status_code)
+    print(response.text)
+    categories = response.json()
+    for category in categories:
+        print(category['name'])
